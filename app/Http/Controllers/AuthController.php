7d008b56ac8f7304aeeff57be7bced968caacdc9
andrii <andrijostrovskij1@gmail.com>
2024-06-20 16:21:44 +0300

    added getvodafonetoken

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,9 +8,11 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\VerificationCode;
-use Vonage\Client;
+// use Vonage\Client;
 use Vonage\SMS\Message\SMS;
 use Vonage\Client\Credentials\Basic;
+
+use GuzzleHttp\Client;
 
 class AuthController extends Controller
 {
@@ -149,5 +151,50 @@ class AuthController extends Controller
         // $verificationCode->delete();
 
         return response()->json(['message' => __('success_verification_code')], 200);
+    }
+
+    public function sendSMSVodafone()
+    {
+        $baseUrl = 'https://a2p.vodafone.ua';
+        $username = '380956139029';
+        $password = 'STRe456892-=wr';
+
+        $client = new \GuzzleHttp\Client();
+
+        try {
+            $response = $client->post("{$baseUrl}/uaa/oauth/token", [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'username' => $username,
+                    'password' => $password,
+                ],
+                'headers' => [
+                    'Authorization' => 'Basic aW50ZXJuYWw6aW50ZXJuYWw=',
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                ],
+            ]);
+
+            $body = $response->getBody();
+            $data = json_decode($body, true);
+
+            $accessToken = $data['access_token'];
+            $refreshToken = $data['refresh_token'];
+
+            return response()->json([
+                'access_token' => $accessToken,
+                'refresh_token' => $refreshToken,
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+
+            return response()->json([
+                'error' => $responseBodyAsString,
+            ], $response->getStatusCode());
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
