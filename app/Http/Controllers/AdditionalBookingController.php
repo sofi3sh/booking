33615634de80e44abd9fc035fadee0b8fd78b474
Max Humennui <max.humennui@gmail.com>
2024-07-01 13:56:51 +0300

    add additional objects automatical available

--- app/Http/Controllers/AdditionalBookingController.php
+++ app/Http/Controllers/AdditionalBookingController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BookingObject;
+use App\Enums\ObjectStatus;
+use App\Enums\ObjectType;
 
 class AdditionalBookingController extends Controller
 {
-    
+        /**
+     * Automatically change the objects status of expired reserved bookings.
+     *
+     * @return void
+     */
+    public static function updateExpiredReservedNotPaidBookingObjectStatus()
+    {
+        $bookedSunbedAndBedObjects = BookingObject::where(function ($query) {
+            $query->where('type', ObjectType::SUNBED->value)
+                  ->orWhere('type', ObjectType::BED->value);
+            })
+            ->where('status', '!=', ObjectStatus::FREE->value)
+            ->count();
+                
+        $allSunbedAndBedObjects = BookingObject::where(function ($query) {
+            $query->where('type', ObjectType::SUNBED->value)
+                  ->orWhere('type', ObjectType::BED->value);
+            })
+            ->count();
+        
+        if ($bookedSunbedAndBedObjects == $allSunbedAndBedObjects) {
+            // TODO
+        }
+    }
 }
